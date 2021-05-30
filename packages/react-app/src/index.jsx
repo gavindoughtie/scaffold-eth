66271502d753b30c7b3c6fe23fa514874d26006a
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-29 17:02:14 -0700

    setting up textile

--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -1,33 +1,71 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import React from "react";
-import { ThemeSwitcherProvider } from "react-css-theme-switcher";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { Buckets, PrivateKey } from '@textile/hub';
+import React from 'react';
+import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { generateKeyText } from './helpers';
+import './index.css';
 
-import { generateKeyText } from "./helpers/crypto";
+const getIdentity = async () => {
+  // : Promise<PrivateKey>
+  /** Restore any cached user identity first */
+  const cached = localStorage.getItem('user-private-identity');
+  if (cached !== null) {
+    /** Convert the cached identity string to a PrivateKey and return */
+    return PrivateKey.fromString(cached);
+  }
+  /** No cached identity existed, so create a new one */
+  const identity = await PrivateKey.fromRandom();
+  /** Add the string copy to the cache */
+  localStorage.setItem('user-private-identity', identity.toString());
+  /** Return the random identity */
+  return identity;
+};
+
+const setup = async (key, identity) => {
+  // Use the insecure key to set up the buckets client
+  const buckets = await Buckets.withKeyInfo(key);
+  // Authorize the user and your insecure keys with getToken
+  await buckets.getToken(identity);
+
+  const result = await buckets.open('io.textile.dropzone');
+  if (!result.root) {
+    throw new Error('Failed to open bucket');
+  }
+  return {
+    buckets: buckets,
+    bucketKey: result.root.key,
+  };
+};
 
 const themes = {
   dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
   light: `${process.env.PUBLIC_URL}/light-theme.css`,
 };
 
-const prevTheme = window.localStorage.getItem("theme");
+const prevTheme = window.localStorage.getItem('theme');
 
-const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract";
+const subgraphUri = 'http://localhost:8000/subgraphs/name/scaffold-eth/your-contract';
 
 const client = new ApolloClient({
   uri: subgraphUri,
   cache: new InMemoryCache(),
 });
 
-generateKeyText().then(keytext => {
+async function init() {
+  const identity = await getIdentity();
+  const textileKey = 'bbaareigmdhkj3shaald5ju7h2e7te5n4z2fhjpu6gehisvokh65m6v4idm'; // ??
+  await setup(textileKey, identity);
+  const keytext = await generateKeyText();
   ReactDOM.render(
     <ApolloProvider client={client}>
-      <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme || "light"}>
+      <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme || 'light'}>
         <App subgraphUri={subgraphUri} keytext={keytext} />
       </ThemeSwitcherProvider>
     </ApolloProvider>,
-    document.getElementById("root"),
+    document.getElementById('root')
   );
-});
+}
+
+init();
